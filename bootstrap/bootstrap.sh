e5d5dc0584a639c9d126412dcd5e3657c0953455
Atilla Gündüz <gunduz.atilla@gmail.com>
2012-10-09 23:17:46 +0300

    syntax errors fixed

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -100,7 +100,7 @@ done"
 # main program
 # chech user 
 if [ "$(id -u)" != "0" ]; then
-	echo “This script must be run as root” 2>&1
+	echo "This script must be run as root" 2>&1
 	echo "sudo ${0}"
 	exit 1
 fi
