aac4c6db01748d55894120b13da04a092ed480dd
Atilla Gündüz <gunduz.atilla@gmail.com>
2012-10-09 18:02:43 +0300

    consolefont.sh functions moved to rc_update.sh

--- bootstrap/rc_update.sh
+++ bootstrap/rc_update.sh
@@ -5,6 +5,25 @@
 # set trq keyboard
 # 
 # VARIABLES
+
+consolefont (){
+	# set consolefont in rc.conf
+	# 
+	CONSOLE=`grep -i "CONSOLEFONT" "${RC}" &> /dev/null ; echo $?`
+	CONSOLE_VAL=`cat "${RC}" | grep -i "CONSOLEFONT" | grep -i "iso09.16" &> /dev/null ; echo $?`
+
+	if [ "${CONSOLE}" == "0" ] ; then
+		if [ "${CONSOLE_VAL}" == "0" ] ; then
+			echo "${RC}: CONSOLEFONT=\"iso09.16\" exists already"
+			else 
+				echo "it exists already a CONSOLEFONT setting in ${RC}"
+		fi
+		else
+			echo "CONSOLEFONT=\"iso09.16\"" >> "${RC}"
+			echo "CONSOLEFONT=\"iso09.16\" added to ${RC}"
+	
+	fi
+}
 keymap(){
 	KEYMAP=`grep -i "KEYMAP" "${RC}" &> /dev/null ; echo $?`
 	KEYMAP_VAL=`cat "${RC}" | grep -i "KEYMAP" | grep -i "trq" &> /dev/null ; echo $?`
@@ -42,6 +61,7 @@ RC="/etc/rc.conf"
 
 # main program
 keymap
+consolefont
 locale
 
 exit 0
