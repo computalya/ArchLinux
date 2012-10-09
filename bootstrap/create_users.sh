c24e74373d625d40dbfe73d4752c9827433f2006
computalya <info@computalya.com>
2012-10-09 13:05:51 +0300

    Update bootstrap/create_users.sh
    version before was not running. this should be okay now

--- bootstrap/create_users.sh
+++ bootstrap/create_users.sh
@@ -2,13 +2,14 @@
 #
 # VARIABLES
 USERNAME="computalya"
+CHECK=`cat /etc/passwd | cut -d":" -f1 | grep "${USERNAME}" &> /dev/null ; echo $?`
 
 # main program
 # if user does not exists...
-if [ "$(id -u)" != "0" ]; then
-	useradd -m -g users -G locate,storage,wheel -s /bin/bash "${USERNAME}"
-	echo "type a password for user : ${USERNAME} 
-	passwd
+if [ "${CHECK}" != "0" ] ; then
+        useradd -m -g users -G locate,storage,wheel -s /bin/bash "${USERNAME}"
+        echo "type a password for user : ${USERNAME}
+        passwd "${USERNAME}"
 fi
 
 exit 0
