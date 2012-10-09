92982f842f2f9c998b12c13a66eabe7776730b65
Atilla Gündüz <gunduz.atilla@gmail.com>
2012-10-09 16:30:51 +0300

    correct path to rc.conf

--- bootstrap/set_keymap.sh
+++ bootstrap/set_keymap.sh
@@ -2,11 +2,11 @@
 # set trq keyboard
 # 
 # VARIABLES
-CHECK_KEYMAP=`grep -i "KEYMAP" rc.conf &> /dev/null ; echo $?`
-CHECK_TRQ=`cat rc.conf | grep -i "KEYMAP" | grep -i "trq" &> /dev/null ; echo $?`
+CHECK_KEYMAP=`grep -i "KEYMAP" /etc/rc.conf &> /dev/null ; echo $?`
+CHECK_TRQ=`cat /etc/rc.conf | grep -i "KEYMAP" | grep -i "trq" &> /dev/null ; echo $?`
 
 if [ "${CHECK_KEYMAP}" == "0" ] && [ "${CHECK_TRQ}" == "0" ] ; then
-	echo "rc.conf: KEYMAP=\"trq\" exists already"
+	echo "/etc/rc.conf: KEYMAP=\"trq\" exists already"
 	else 
 		echo "KEYMAP=\"trq\"" >> rc.conf
 fi
