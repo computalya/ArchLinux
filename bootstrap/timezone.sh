34139abea4ddfc1906a9d9f877ff37b0337189fc
Atilla Gündüz <gunduz.atilla@gmail.com>
2012-10-09 16:06:01 +0300

    timezone.sh - first running version

--- bootstrap/timezone.sh
+++ bootstrap/timezone.sh
@@ -3,34 +3,10 @@
 # 
 # VARIABLES
 if [ ! -e /etc/localtime ] ; then
-	echo "/etc/localtime yok"
-fi
-
-
-exit 1
-USERNAME="computalya"
-CHECK=`cat /etc/passwd | cut -d":" -f1 | grep "${USERNAME}" &> /dev/null ; echo $?`
-CHECK_SUDO=`cat /etc/passwd | cut -d":" -f1 | grep "${USERNAME}" &> /dev/null ; echo $?`
-
-# main program
-# if user does not exists...
-if [ "${CHECK}" != "0" ] ; then
-        useradd -m -g users -G locate,storage,wheel -s /bin/bash "${USERNAME}"
-        echo "type a password for user : ${USERNAME}"
-        passwd "${USERNAME}"
+	ln -s /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
+	echo "/etc/localtime has been linked to /usr/share/zoneinfo/Europe/Istanbul"
 	else
-		echo "user ${USERNAME} exists already. nothing to do"
+		echo "error: /etc/localtime exists already"
 fi
-
-# add user to sudoer
-# check whether sudo is installed
-if [ "${CHECK_SUDO}" != "0" ] ; then
-	echo "sudo is not installed. do pacman -S sudo"
-	exit 1
-	else
-		echo "${USERNAME} ALL=(ALL) ALL" >> /etc/sudoers
-		echo "sudoers udpated"
-fi
-
 
 exit 0
