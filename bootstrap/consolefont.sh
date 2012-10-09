e7f4be3856ac4dea68195cce93443b4df8bcc569
Atilla Gündüz <gunduz.atilla@gmail.com>
2012-10-09 16:57:42 +0300

    improved consolefont.sh

--- bootstrap/consolefont.sh
+++ bootstrap/consolefont.sh
@@ -5,6 +5,19 @@
 CHECK_CONSOLE=`grep -i "CONSOLEFONT" /etc/rc.conf &> /dev/null ; echo $?`
 CHECK_ISO=`cat /etc/rc.conf | grep -i "CONSOLEFONT" | grep -i "iso09.16" &> /dev/null ; echo $?`
 
+if [ "${CHECK_CONSOLE}" == "0" ] ; then
+	if [ "${CHECK_ISO}" == "0" ] ; then
+		echo "/etc/rc.conf: CONSOLEFONT=\"iso09.16\" exists already"
+		exit 1
+		else 
+			echo "CONSOLEFONT=\"iso09.16\"" >> /etc/rc.conf
+	fi
+	else
+		echo "it exists already a CONSOLEFONT setting in /etc/rc.conf"
+	
+fi
+
+exit 0
 if [ "${CHECK_CONSOLE}" == "0" ] && [ "${CHECK_ISO}" == "0" ] ; then
 	echo "/etc/rc.conf: CONSOLEFONT=\"iso09.16\" exists already"
 	else 
