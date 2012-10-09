2e02caba4bc60ffbc06a50202b0621b814d08c0f
Atilla Gündüz <gunduz.atilla@gmail.com>
2012-10-09 15:16:45 +0300

    added sudoers to create_users.sh

--- bootstrap/create_users.sh
+++ bootstrap/create_users.sh
@@ -1,8 +1,10 @@
 #/etc/bin/bash
-#
+# create a user and give sudo permissions
+# 
 # VARIABLES
 USERNAME="computalya"
 CHECK=`cat /etc/passwd | cut -d":" -f1 | grep "${USERNAME}" &> /dev/null ; echo $?`
+CHECK_SUDO=`cat /etc/passwd | cut -d":" -f1 | grep "${USERNAME}" &> /dev/null ; echo $?`
 
 # main program
 # if user does not exists...
@@ -13,5 +15,15 @@ if [ "${CHECK}" != "0" ] ; then
 	else
 		echo "user ${USERNAME} exists already. nothing to do"
 fi
+
+# add user to sudoer
+# check whether sudo is installed
+if [ "${CHECK_SUDO}" != "0" ] ; then
+	echo "sudo is not installed. do pacman -S sudo"
+	exit 1
+	else
+		echo "${USERNAME} ALL=(ALL) ALL" >> /etc/sudoers
+fi
+
 
 exit 0
