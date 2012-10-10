e3ceef522b7189df9e99809ba4b69cd3f95293d4
Atilla Gündüz <gunduz.atilla@gmail.com>
2012-10-10 13:25:36 +0300

    .

--- bootstrap/openssh_conf.sh
+++ bootstrap/openssh_conf.sh
@@ -35,7 +35,7 @@ if [ -f "${HOME}/.ssh/id_rsa.pub" ] ; then
 		echo "copy your publich id -id_rsa.pub- to $HOME/.ssh"
 		echo "from the machine where the key is located"
 #		echo "cat ~/.ssh/*.pub | ssh root@$IP 'cat>>.ssh/authorized_keys'"
-		echo "scp /.ssh/*.pub root@192.168.1.26:/root
+		echo "scp ~/.ssh/*.pub root@192.168.1.26:/root"
 		exit 1
 fi
 
