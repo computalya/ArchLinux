886ee86216367c0e0d2b3451ced3a2574f0b92d2
Atilla Gündüz <gunduz.atilla@gmail.com>
2012-10-10 13:37:29 +0300

    openssh_conf.sh -> added authorized_keys

--- bootstrap/openssh_conf.sh
+++ bootstrap/openssh_conf.sh
@@ -32,12 +32,10 @@ if [ -f "${HOME}/id_rsa.pub" ] ; then
 	echo "if it is NOT the correct one CTRL+C"
 	echo "if it is the correct one ENTER to continue..."
 	read X
-	echo "ok"
-	exit 1
+
 	else
 		echo "copy your publich id -id_rsa.pub- to $HOME/.ssh"
 		echo "from the machine where the key is located"
-#		echo "cat ~/.ssh/*.pub | ssh root@$IP 'cat>>.ssh/authorized_keys'"
 		echo "scp ~/.ssh/*.pub root@192.168.1.26:/root"
 		exit 1
 fi
@@ -49,9 +47,13 @@ for i in `echo $SSH_USERS` ; do
 	if [ ! -d "${HOME_DIR}/.ssh" ] ; then
 		su "${i}" -c "mkdir $HOME_DIR/.ssh"
 		echo "$HOME_DIR created"
+
+		su "${i}" -c "cat /root/id_rsa.pub >> $HOME_DIR/.ssh/authorized_keys"
 		else
 			echo "info -> $HOME_DIR/.ssh exist already"
 	fi
 done
+# rm /root/id_rsa.pub
+# echo "/root/id_rsa.pub removed"
 
 exit 0
