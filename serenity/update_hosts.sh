582a40d4f5d03e96ed11ff475ff7cc511efca8a2
Atilla Gündüz <gunduz.atilla@gmail.com>
2012-10-18 16:49:57 +0300

    update_hosts.sh bug fix

--- serenity/update_hosts.sh
+++ serenity/update_hosts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # /etc/httpd/conf/extra/vhosts.d/*.conf
-for i in `ls -1 /etc/httpd/conf/extra/vhosts.d/*.conf | sed 's/.conf//'` ; do
-	# echo $i | sed 's/.conf//'
-
-	echo -e "127.0.0.1\t$i www.$i" 
+for i in `ls -1 /etc/httpd/conf/extra/vhosts.d/*.conf` ; do
+	FILE=`basename $i | sed 's/.conf//'` 
+	echo -e "127.0.0.1\t$FILE www.$FILE" &>> /etc/hosts
 done
